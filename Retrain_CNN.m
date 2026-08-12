34e6080b03540d5b3c51c5460190eeaf5719df25
Milja Harju <miljaemilia.harju@hotmail.fi>
2026-08-12 15:33:01 +0300

    Add extra samples from problematic channels

--- Retrain_CNN.m
+++ Retrain_CNN.m
@@ -1,14 +1,14 @@
-%% Split data into training & testing & validation sets using 80/20/20 ratio
+%% Split data into training & testing & validation sets using 80/10/10 ratio
 
 % number of samples to each group
-n_train_TN = ceil(size(segment_TN,3)*0.8);
-n_test_TN = (size(segment_TN,3)-n_train_TN)*0.5;
+n_train_TN = 285;
+n_test_TN = 35;
 
-n_train_TP = ceil(size(segment_TP,3)*0.8);
-n_test_TP = (size(segment_TP,3)-n_train_TP)*0.5; % = n_val
+n_train_TP = 56;
+n_test_TP = 7; % = n_val
 
-n_train_path = n_train_TN / 2;
-n_test_path = n_test_TN / 2;
+n_train_extra = 'fill';
+n_test_extra = 'fill';
 
 % shuffle segments and pick the samples
 segment_TN = segment_TN(:,:,randperm(size(segment_TN,3)));
@@ -21,31 +21,36 @@ TP_train = segment_TP(:,:,1:n_train_TP);
 TP_test = segment_TP(:,:,n_train_TP+1:n_train_TP+n_test_TP);
 TP_val = segment_TP(:,:,n_train_TP+n_test_TP+1:end);
 
-segment_path = segment_pathology(:,:,randperm(size(segment_pathology,3)));
-pathology_train = segment_pathology(:,:,1:n_train_path);
-pathology_test = segment_pathology(:,:,n_train_path+1:n_train_path+n_test_path);
-pathology_val = segment_pathology(:,:,n_train_path+n_test_path+1:end);
+segment_extra = segment_extra(:,:,randperm(size(segment_extra,3)));
+TP_train = segment_extra(:,:,1:n_train_extra);
+TP_test = segment_extra(:,:,n_train_extra+1:n_train_extra+n_test_extra);
+TP_val = segment_extra(:,:,n_train_extra+n_test_extra+1:end);
 
 % combine to X and y
-X_train = cat(3,TN_train, TP_train, pathology_train);
-y_train = [(repmat([2],1,n_train_TN)),(repmat([1],1,n_train_TP)),(repmat([3],1,n_train_path))];
+% ADD EXTRA SAMPLES HERE!!!
+X_train = cat(3,TN_train, TP_train );
+y_train = [(repmat([2],1,n_train_TN)),(repmat([1],1,n_train_TP)),3];
+y_train = categorical(y_train, [1 2 3], {'noise','ok','patology'});
+y_train = y_train(1:end-1);
 
-X_test= cat(3,TN_test, TP_test, pathology_test);
-y_test= [(repmat([2],1,n_test_TN)),(repmat([1],1,n_test_TP)),(repmat([3],1,n_test_path))];
+X_test= cat(3,TN_test, TP_test);
+y_test= [(repmat([2],1,n_test_TN)),(repmat([1],1,n_test_TP)),3];
+y_test = categorical(y_test, [1 2 3], {'noise','ok','patology'});
+y_test = y_test(1:end-1);
 
-X_val = cat(3,TN_val, TP_val, pathology_val);
-y_val = [(repmat([2],1,n_test_TN)),(repmat([1],1,n_test_TP)),(repmat([3],1,n_test_path))];
+X_val = cat(3,TN_val, TP_val);
+y_val = [(repmat([2],1,n_test_TN)),(repmat([1],1,n_test_TP)),3];
+y_val = categorical(y_val, [1 2 3], {'noise','ok','patology'});
+y_val = y_val(1:end-1);
+
+% shape the samples
+X_train = reshape(X_train, size(X_train,1), size(X_train,2), 1, size(X_train,3));
+X_val = reshape(X_val, size(X_val,1), size(X_val,2), 1, size(X_val,3));
+X_test = reshape(X_test, size(X_test,1), size(X_test,2), 1, size(X_test,3));
 
 % retraining the network
 load('convnet.mat')
-layers = convnet.Layers;
-
-% shape the samples and labels
-X_train = reshape(X_train, size(X_train,1), size(X_train,2), 1, size(X_train,3));
-y_train = categorical(y_train, [1 2 3], {'noise','ok','patology'});
-
-X_val = reshape(X_val, size(X_val,1), size(X_val,2), 1, size(X_val,3));
-y_val = categorical(y_val, [1 2 3], {'noise','ok','patology'});
+layers = convnet.Layers; % get original layers
 
 options = trainingOptions('sgdm', ...
     'InitialLearnRate', 1e-5, ...   % gentle nudge, low LR
@@ -58,13 +63,10 @@ options = trainingOptions('sgdm', ...
     'ValidationPatience',5,...
     'Plots','training-progress');
 
+% retraining
 net = trainNetwork(X_train, y_train, layers, options);
 
-%
-X_test = reshape(X_test, size(X_test,1), size(X_test,2), 1, size(X_test,3));
-y_test = categorical(y_test, [1 2 3], {'noise','ok','patology'});
-
-
+% predictions before and after
 YPredBefore = classify(convnet, X_test);            % original network
 YPredAfter = classify(net, X_test);     % fine-tuned network
 
@@ -106,3 +108,5 @@ comparisonTable = table(classNames', ...
         'Precision_Before','Precision_After','Precision_Delta', ...
         'Recall_Before','Recall_After','Recall_Delta', ...
         'F1_Before','F1_After','F1_Delta'})
+
+save("retrained_network","net")
