f34885c72532f7c87aa4a8d36aac22c03f8d5b37
Milja Harju <miljaemilia.harju@hotmail.fi>
2026-08-14 10:59:16 +0300

    -

--- Retrain_CNN.m
+++ Retrain_CNN.m
@@ -1,14 +1,16 @@
+rng(67)
+
 %% Split data into training & testing & validation sets using 80/10/10 ratio
 
 % number of samples to each group
 n_train_TN = 285;
 n_test_TN = 35;
 
-n_train_TP = 56;
+n_train_TP = 57;
 n_test_TP = 7; % = n_val
 
-n_train_extra = 'fill';
-n_test_extra = 'fill';
+n_train_extra = 285;
+n_test_extra = 35;
 
 % shuffle segments and pick the samples
 segment_TN = segment_TN(:,:,randperm(size(segment_TN,3)));
@@ -22,24 +24,24 @@ TP_test = segment_TP(:,:,n_train_TP+1:n_train_TP+n_test_TP);
 TP_val = segment_TP(:,:,n_train_TP+n_test_TP+1:end);
 
 segment_extra = segment_extra(:,:,randperm(size(segment_extra,3)));
-TP_train = segment_extra(:,:,1:n_train_extra);
-TP_test = segment_extra(:,:,n_train_extra+1:n_train_extra+n_test_extra);
-TP_val = segment_extra(:,:,n_train_extra+n_test_extra+1:end);
+extra_train = segment_extra(:,:,1:n_train_extra);
+extra_test = segment_extra(:,:,n_train_extra+1:n_train_extra+n_test_extra);
+extra_val = segment_extra(:,:,n_train_extra+n_test_extra+1:end);
 
 % combine to X and y
 % ADD EXTRA SAMPLES HERE!!!
-X_train = cat(3,TN_train, TP_train );
-y_train = [(repmat([2],1,n_train_TN)),(repmat([1],1,n_train_TP)),3];
+X_train = cat(3,TN_train, TP_train, extra_train );
+y_train = [(repmat([2],1,n_train_TN)),(repmat([1],1,n_train_TP)),repmat([2],1,n_train_extra),3];
 y_train = categorical(y_train, [1 2 3], {'noise','ok','patology'});
 y_train = y_train(1:end-1);
 
-X_test= cat(3,TN_test, TP_test);
-y_test= [(repmat([2],1,n_test_TN)),(repmat([1],1,n_test_TP)),3];
+X_test= cat(3,TN_test, TP_test, extra_test);
+y_test= [(repmat([2],1,n_test_TN)),(repmat([1],1,n_test_TP)),repmat([2],1,n_test_extra),3];
 y_test = categorical(y_test, [1 2 3], {'noise','ok','patology'});
 y_test = y_test(1:end-1);
 
-X_val = cat(3,TN_val, TP_val);
-y_val = [(repmat([2],1,n_test_TN)),(repmat([1],1,n_test_TP)),3];
+X_val = cat(3,TN_val, TP_val, extra_val);
+y_val = [(repmat([2],1,n_test_TN)),(repmat([1],1,n_test_TP)),repmat([2],1,n_test_extra),3];
 y_val = categorical(y_val, [1 2 3], {'noise','ok','patology'});
 y_val = y_val(1:end-1);
 
@@ -59,7 +61,7 @@ options = trainingOptions('sgdm', ...
     'Momentum', 0.9, ...
     'L2Regularization', 0.001, ...
     'ValidationData', {X_val, y_val}, ...
-    'ValidationFrequency', 50, ...
+    'ValidationFrequency', 10, ...
     'ValidationPatience',5,...
     'Plots','training-progress');
 
@@ -68,7 +70,7 @@ net = trainNetwork(X_train, y_train, layers, options);
 
 % predictions before and after
 YPredBefore = classify(convnet, X_test);            % original network
-YPredAfter = classify(net, X_test);     % fine-tuned network
+YPredAfter = classify(net2, X_test);     % fine-tuned network
 
 confusionmat(y_test, YPredBefore)
 confusionmat(y_test, YPredAfter)
@@ -109,4 +111,4 @@ comparisonTable = table(classNames', ...
         'Recall_Before','Recall_After','Recall_Delta', ...
         'F1_Before','F1_After','F1_Delta'})
 
-save("retrained_network","net")
+save("retrained_network1","net")
