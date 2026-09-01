6649eedbf7c207fb60621926b1579c3347d91b5c
Milja Harju <miljaemilia.harju@hotmail.fi>
2026-09-01 11:47:05 +0300

    Add LOSO training and validation

--- retraining.m
+++ retraining.m
@@ -1,14 +1,3 @@
-%{
-artefacts = load("extracted_artefacts.mat");
-controls = load("extracted_good_samples.mat");
-good_nums = load("patient_number_good.mat");
-controls.ids = good_nums.patient_number;
-art_ids = load("patient_number_artefacts.mat");
-artefacts.ids = art_ids.patient_number; 
-
-clear art_ids good_nums
-%}
-
 tmp = load("extracted_artefacts.mat");
 artefacts.extracted_samples = single(tmp.extracted_samples);
 clear tmp
@@ -27,7 +16,7 @@ clear tmp
 
 %%
 load("convnet.mat")
-%
+%%
 patients = unique(artefacts.ids);
 n = numel(patients);
 
@@ -59,15 +48,17 @@ controls_idx = [];
 for i = 1:numel(patients)
     % artefacts
     idx = find(artefacts.ids == patients(i));
-    idx_first_n = idx(1:n_samples); 
-    artefacts_idx = [artefacts_idx, idx_first_n];
-    selected_artefacts(:,:,end+1:end+22) = artefacts.extracted_samples(:,:,idx_first_n);
+    rng(67);
+    idx_n = randperm(length(idx), n_samples); 
+    artefacts_idx = [artefacts_idx, idx_n];
+    selected_artefacts(:,:,end+1:end+22) = artefacts.extracted_samples(:,:,idx_n);
 
-    % ctonrols
+    % controls
     idx = find(controls.ids == patients(i));
-    idx_first_n = idx(1:n_samples); 
-    controls_idx = [controls_idx, idx_first_n];
-    selected_controls(:,:,end+1:end+22) = controls.extracted_samples(:,:,idx_first_n);
+    rng(68)
+    idx_n =  randperm(length(idx), n_samples); 
+    controls_idx = [controls_idx, idx_n];
+    selected_controls(:,:,end+1:end+22) = controls.extracted_samples(:,:,idx_n);
 end
 
 
@@ -98,11 +89,11 @@ net = trainNetwork(X_train, Y_train, layers, options);
 clear X_train Y_train selected_artefacts selected_controls
 
 % free RAM by removing the samples you already copied into X_train
-artefacts.extracted_samples(:,:,artefacts_idx) = [];
-artefacts.ids(artefacts_idx) = [];
+%artefacts.extracted_samples(:,:,artefacts_idx) = [];
+%artefacts.ids(artefacts_idx) = [];
 
-controls.extracted_samples(:,:,controls_idx) = [];
-controls.ids(controls_idx) = [];
+%controls.extracted_samples(:,:,controls_idx) = [];
+%controls.ids(controls_idx) = [];
 %% leave out the samples used for training
 
 testset_artefacts_mask = true(numel(artefacts.ids), 1);
@@ -180,5 +171,132 @@ ylim([-0.05 1.05])
 
 fprintf("Accuracy mean: %.3f, PPV mean: %.3f, Specificity mean: %.3f\n",mean(acc), mean(ppv), mean(spec))
 
-%
+%% LOSO
+layers = convnet.Layers; % get original layers
+
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate', 1e-4, ...   % gentle nudge, low LR
+    'MaxEpochs', 10, ...
+    'MiniBatchSize', 64, ...
+    'Momentum', 0.9, ...
+    'L2Regularization', 0.01, ...
+    'Plots','none');
+
+I = repmat(1:n,n,1)';
+testing_idx = I(logical(eye(n)));
+training_idx = reshape(I(~eye(n)), n-1, n)';
+
+results = struct();
+
+for k = 1:n % k is the leave-out patient
+    k_th = patients(k); % loso subject
+    others = patients(training_idx(k,:));
+    % --- Training set ---
+    % form the training set with n-1 subjects
+    fold_mask_artifacts = ismember(artefacts.ids, others); %& testset_artefacts_mask;
+    fold_artifacts = artefacts.extracted_samples(:,:,fold_mask_artifacts);
+    fold_artifacts_id = artefacts.ids(fold_mask_artifacts);
+    fold_mask_controls = ismember(controls.ids, others); % & testset_controls_mask;
+    fold_controls = controls.extracted_samples(:,:,fold_mask_controls);
+    fold_controls_id = controls.ids(fold_mask_controls);
+
+    % select n_samples from each subject ("random")
+    selected_artefacts = zeros(15000,5,0);
+    selected_controls = zeros(15000,5,0);
+    for i = 1:numel(training_idx(k,:))
+        % artefacts
+        idx = find(fold_artifacts_id == others);
+        rng(67);
+        idx_n = randperm(length(idx), n_samples); 
+        selected_artefacts(:,:,end+1:end+22) = artefacts.extracted_samples(:,:,idx_n);
+    
+        % controls
+        idx = find(fold_controls_id == others);
+        rng(68)
+        idx_n =  randperm(length(idx), n_samples); 
+        selected_controls(:,:,end+1:end+22) = controls.extracted_samples(:,:,idx_n);
+    end
+
+    % form the training set
+    X_train = cat(3, selected_artefacts, selected_controls);
+    X_train = reshape(X_train, size(X_train,1), size(X_train,2), 1, size(X_train,3));
+    Y_train = [repmat(1,1,size(selected_artefacts,3)), repmat(2,1,size(selected_controls,3))];
+    Y_train = categorical(Y_train, [1 2 3], {'noise','ok','patology'});
+    fprintf("Size of X_train: %d \n", size(X_train,3))
+
+    % --- Retrain the CNN using x_train and y_train ---
+    net = trainNetwork(X_train, Y_train, layers, options);
+    fprintf("Network training successful!\n")
+   
+    % --- Testing set ---
+    % select the kth subjects samples
+    fold_mask_artifacts = ismember(artefacts.ids, k_th); %& testset_artefacts_mask;
+    fold_artifacts = artefacts.extracted_samples(:,:,fold_mask_artifacts);
+    fold_artifacts_id = artefacts.ids(:,:,fold_mask_artifacts);
+    fold_mask_controls = ismember(controls.ids, k_th); % & testset_controls_mask;
+    fold_controls = controls.extracted_samples(:,:,fold_mask_controls);
+    fold_controls_id = controls.ids(:,:,fold_mask_controls);
+
+    % select random 22 samples
+    % artefacts
+    idx = find(fold_artifacts_id == k_th);
+    rng(67);
+    idx_n = randperm(length(idx), n_samples); 
+    selected_artefacts = artefacts.extracted_samples(:,:,idx_n);
+
+    % controls
+    idx = find(fold_controls_id == k_th);
+    rng(68)
+    idx_n =  randperm(length(idx), n_samples); 
+    selected_controls = controls.extracted_samples(:,:,idx_n);
+
+
+    X_test = cat(3, selected_artefacts, selected_controls);
+    X_test = reshape(X_test, size(X_test,1), size(X_test,2), 1, size(X_test,3));
+    Y_test = [repmat(1,1,size(selected_artefacts,3)), repmat(2,1,size(selected_controls,3))];
+    Y_test = categorical(Y_test, [1 2 3], {'noise','ok','patology'});
+    fprintf("Size of testing set: %d\n", size(X_test,3))
+
+    % --- Test the network ---
+    Y_pred = classify(net, X_test);
+    Y_pred  = mergecats(Y_pred,  {'ok','patology'}, 'ok');
+    acc = mean(Y_pred == Y_test');
+    fprintf('Fold %d test accuracy: %.3f\n', k, acc); 
+
+    % --- Save performance and net (?) ---
+    results{k}.Y_true = Y_test;
+    results{k}.Y_pred = Y_pred;
+    results{k}.acc = acc;
+    results{k}.net = net;
+end
+
+acc = zeros(1,n);
+ppv = zeros(1,n);
+spec = zeros(1,n);
+for i = 1:n
+    acc(i) = results{i}.acc;
+    Y_true = results{i}.Y_true;
+    Y_pred = results{i}.Y_pred;
+
+    TP = sum(Y_true == 'noise' & Y_pred' == 'noise');
+    FP = sum(Y_true == 'ok' & Y_pred' == 'noise');
+    FN = sum(Y_true == 'noise' & Y_pred' == 'ok');
+    TN = sum(Y_true == 'ok' &  Y_pred' == 'ok');
+    ppv(i) = TP/(TP+FP); % noise actually being noise
+    spec(i) = TN/(TN+FP); % ok classified as ok
+end
+
+figure;
+plot(1:n, acc, '-o', 'MarkerFaceColor','auto'); hold on
+plot(1:n, ppv, '-o', 'MarkerFaceColor','auto');
+plot(1:n, spec, '-o', 'MarkerFaceColor','auto');
+xticks(1:n)
+xticklabels(patients)
+title("Per-subject testing of retrained CNN");
+xlabel("Subject")
+ylabel("Metrics")
+legend("Accuracy", "PPV", "Specificity", Location="southeast")
+ylim([-0.05 1.05])
+
+fprintf("Accuracy mean: %.3f, PPV mean: %.3f, Specificity mean: %.3f\n",mean(acc), mean(ppv), mean(spec))
 
